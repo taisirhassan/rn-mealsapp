25de1fcdf306274db91b1a872276577b6d48ed5c
Taisir <taisirisawesome@gmail.com>
2022-03-01 14:47:52 -0500

    New Meals App Initialized

---
+++ models/category.js
@@ -0,0 +1,9 @@
+class Category {
+  constructor(id, title, colour) {
+    this.id = id;
+    this.title = title;
+    this.colour = colour;
+  }
+}
+
+export default Category;
---
+++ screens/MealDetailScreen.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
+
+const MealDetailScreen = (props) => {
+  return (
+    <View style={styles.screen}>
+      <Text> The Meal Detail Screen!</Text>
+      <Button
+        title="Go Back to Categories"
+        onPress={() => {
+          props.navigation.popToTop();
+        }}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default MealDetailScreen;
